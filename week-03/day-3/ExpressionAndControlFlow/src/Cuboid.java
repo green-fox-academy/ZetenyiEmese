b830c2fc33881723354e2250274ddaf9681db649
Zétényi Emese <zetenyi.emese@gmail.com>
2018-10-18 19:28:36 +0200

    Modifying the declaration of variables

--- week-03/day-3/ExpressionAndControlFlow/src/Cuboid.java
+++ week-03/day-3/ExpressionAndControlFlow/src/Cuboid.java
@@ -1,12 +1,11 @@
 public class Cuboid {
     public static void main(String[] args) {
-        double side1, side2, side3, surfaceArea, volume;
-        side1=25.89;
-        side2=64.45;
-        side3=37.62;
+        double side1 = 25.89;
+        double side2 = 78.24;
+        double side3 = 37.62;
 
-        surfaceArea=2*(side1*side2+side1*side3+side2*side3);
-        volume=side1*side2*side3;
+        double surfaceArea = 2*(side1*side2+side1*side3+side2*side3);
+        double volume = side1*side2*side3;
 
         System.out.println("Surface area: " + surfaceArea);
         System.out.println("Volume: " + volume);

fe2d467cbb27925f5ea742a1026da33c0075e7ed
Zétényi Emese <zetenyi.emese@gmail.com>
2018-10-20 22:05:08 +0200

    White spaces around operators

--- week-03/day-3/ExpressionAndControlFlow/src/FizzBuzz.java
+++ week-03/day-3/ExpressionAndControlFlow/src/FizzBuzz.java
@@ -1,12 +1,12 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i=0; i<100; i++){
-            int number = i+1;
-            if (number%3==0 && number%5==0) {
+        for (int i = 0; i < 100; i++){
+            int number = i + 1;
+            if (number % 3 == 0 && number % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (number%3==0){
+            } else if (number % 3 == 0){
                 System.out.println("Fizz");
-            } else if (number%5==0){
+            } else if (number % 5 == 0){
                 System.out.println("Buzz");
             } else {
                 System.out.println(number);

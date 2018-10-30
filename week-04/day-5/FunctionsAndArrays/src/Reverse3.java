fdd682cd1a51e91568d2fde71a9a2d195b70397e
Zétényi Emese <zetenyi.emese@gmail.com>
2018-10-30 01:09:45 +0100

    A third version

--- week-04/day-5/FunctionsAndArrays/src/Reverse3.java
+++ week-04/day-5/FunctionsAndArrays/src/Reverse3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Reverse3 {
+    public static void main(String[] args) {
+        int[] aj = {3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(aj));
+        reverse(aj);
+        System.out.println(Arrays.toString(aj));
+    }
+
+    //Reverses the elements in the array so that the original array changes:
+    public static void reverse(int[] array) {
+        for (int i = 0, j = array.length - 1; i < j; i++, j--) {
+            int item = array[i];
+            array[i] = array[j];
+            array[j] = item;
+        }
+    }
 }

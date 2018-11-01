bc73ea9f9b5532955167edfe71a92448c55d150e
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-01 23:01:19 +0100

    Comments added

--- week-04/day-5/FunctionsAndArrays/src/Reverse2.java
+++ week-04/day-5/FunctionsAndArrays/src/Reverse2.java
@@ -4,16 +4,16 @@ public class Reverse2 {
     public static void main(String[] args) {
         int[] aj = {3, 4, 5, 6, 7};
         System.out.println(Arrays.toString(aj));
-        System.out.println(Arrays.toString(createsAReversedArray(aj)));
-        System.out.println(Arrays.toString(aj));
+        System.out.println(Arrays.toString(reverseArray(aj)));
+        System.out.println(Arrays.toString(aj)); //the original array is not changed
     }
 
     // Creates a new array with the reversed elements, but the original array remains the same:
-    public static int[] createsAReversedArray(int[] array){
-        int[] reverseArray = new int[array.length];
+    public static int[] reverseArray(int[] array){
+        int[] revArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            reverseArray[i] = array[array.length-1-i];
+            revArray[i] = array[array.length-1-i];
         }
-        return reverseArray;
+        return revArray;
     }
 }

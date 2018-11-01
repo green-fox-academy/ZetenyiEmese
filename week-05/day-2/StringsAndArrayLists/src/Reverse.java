e3bed470cb1724622a288f0274548e7157298fad
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-01 22:54:03 +0100

    Comments added

--- week-05/day-2/StringsAndArrayLists/src/Reverse.java
+++ week-05/day-2/StringsAndArrayLists/src/Reverse.java
@@ -3,6 +3,7 @@ public class Reverse {
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
         System.out.println(reversed);
         System.out.println(reverse(reversed));
+        System.out.println(reversed); //string reversed is not changed
     }
 
     public static String reverse(String text){

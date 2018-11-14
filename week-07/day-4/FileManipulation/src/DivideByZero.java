86c5e00eefc0446bc530782f27739929edb71898
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-15 00:09:29 +0100

    dividing by zero

---
+++ week-07/day-4/FileManipulation/src/DivideByZero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+    public static void main(String[] args) {
+        // Create a function that takes a number
+        // divides ten with it,
+        // and prints the result.
+        // It should print "fail" if the parameter is 0
+
+        Scanner intInput = new Scanner(System.in);
+        int n = intInput.nextInt();
+
+        divideTenByNumber(n);
+    }
+
+    public static void divideTenByNumber(int n) {
+        try {
+            System.out.println(10 / n);
+        } catch (ArithmeticException e) {
+            System.out.println("Fail!");
+        }
+    }
+}
+
+

6f67039d266dd33130bad3cf1f593cd2642ee107
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-23 21:39:53 +0100

    class FarmMain with main method completed

--- week-08/day-2/Farm/src/FarmMain.java
+++ week-08/day-2/Farm/src/FarmMain.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class FarmMain {
+    public static void main(String[] args) {
+        Farm farm = new Farm();
+        System.out.println(farm);
+
+        farm.feed();
+        System.out.println(farm);
+
+        farm.breed();
+        farm.breed();
+        farm.breed();
+        System.out.println(farm);
+
+        Animal anAnimal1 = farm.getAnimals().get(1);
+        farm.feed(anAnimal1);
+
+        Animal anAnimal2 = farm.getAnimals().get(3);
+        farm.feed(anAnimal2);
+        farm.feed(anAnimal2);
+
+        System.out.println(farm);
+
+        farm.slaughter();
+        System.out.println(farm);
+
+        farm.slaughter();
+        System.out.println(farm);
+    }
 }

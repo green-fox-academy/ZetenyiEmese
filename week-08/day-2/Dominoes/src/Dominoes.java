7c5480b274d421340da257b8b065b08c21f2a6ac
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-23 17:10:27 +0100

    whitespaces added

--- week-08/day-2/Dominoes/src/Dominoes.java
+++ week-08/day-2/Dominoes/src/Dominoes.java
@@ -10,17 +10,20 @@ public class Dominoes {
         List<Domino> dominoes = initializeDominoes();
 
         System.out.println(dominoes);
-        System.out.println(order(dominoes));
+        order(dominoes);
+        System.out.println(dominoes);
     }
 
-    static List<Domino> initializeDominoes() {
+    public static List<Domino> initializeDominoes() {
         List<Domino> dominoes = new ArrayList<>();
+
         dominoes.add(new Domino(5, 2));
         dominoes.add(new Domino(4, 6));
         dominoes.add(new Domino(1, 5));
         dominoes.add(new Domino(6, 7));
         dominoes.add(new Domino(2, 4));
         dominoes.add(new Domino(7, 1));
+
         return dominoes;
     }
 

ccc01f5147ae550e4c7ec95899034a48e2f5a4ef
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-23 17:06:29 +0100

    class Sharpie completed

---
+++ week-08/day-2/SharpieSet/src/Sharpie.java
@@ -0,0 +1,28 @@
+public class Sharpie {
+    private String color;
+    private float width;
+    private float inkAmount;
+
+    public Sharpie(String color, float width) {
+        this.color = color;
+        this.width = width;
+        this.inkAmount = 100f;
+    }
+
+    public float getInkAmount() {
+        return inkAmount;
+    }
+
+    public void use() {
+        inkAmount -= 10;
+    }
+
+    @Override
+    public String toString() {
+        return "Sharpie{" +
+                "color='" + color + '\'' +
+                ", width=" + width +
+                ", inkAmount=" + inkAmount +
+                '}';
+    }
+}

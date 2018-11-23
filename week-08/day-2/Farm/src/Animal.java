1e8b0ab83dee74111919537af2ecd3139039ccbb
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-23 21:38:52 +0100

    class Animal completed

---
+++ week-08/day-2/Farm/src/Animal.java
@@ -0,0 +1,34 @@
+public class Animal {
+    private int hunger;
+    private int thirst;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void eat(){
+        hunger--;
+    }
+
+    public void drink(){
+        thirst--;
+    }
+
+    public void play(){
+        hunger++;
+        thirst++;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "hunger=" + hunger +
+                ", thirst=" + thirst +
+                '}';
+    }
+}

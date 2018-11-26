77ac44deb983c15c63f2f6bde1eb60525748d9d8
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-26 22:37:29 +0100

    exercise Power completed

---
+++ week-09/day-2/Recursion/src/Power.java
@@ -0,0 +1,13 @@
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(powerN(5, 3));
+    }
+
+    public static int powerN(int base, int n){
+        if (n == 1){
+            return base;
+        } else {
+            return base * powerN(base, n - 1);
+        }
+    }
+}

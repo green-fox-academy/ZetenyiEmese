923f767c8a1e85019b53d58e2d10fb054554eed6
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-26 21:56:06 +0100

    exercise Counter completed

---
+++ week-09/day-2/Recursion/src/Counter.java
@@ -0,0 +1,21 @@
+public class Counter {
+    public static void main(String[] args) {
+        countDown(10);
+        System.out.println();
+        countUp(10);
+    }
+
+    public static void countDown(int n){
+        System.out.println(n);
+        if (n > 0) {
+            countDown(n - 1);
+        }
+    }
+
+    public static void countUp(int n){
+        if (n > 0) {
+            countUp(n - 1);
+        }
+        System.out.println(n);
+    }
+}

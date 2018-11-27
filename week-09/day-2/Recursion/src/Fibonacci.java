7b045d2a36ef28263bb9e4b56bfac36487d11b33
Zétényi Emese <zetenyi.emese@gmail.com>
2018-11-27 23:01:06 +0100

    added whitespaces

--- week-09/day-2/Recursion/src/Fibonacci.java
+++ week-09/day-2/Recursion/src/Fibonacci.java
@@ -4,10 +4,10 @@ public class Fibonacci {
         System.out.println(fibonacci(8)); // 21
     }
 
-    public static int fibonacci(int n){
-        if (n == 0){
+    public static int fibonacci(int n) {
+        if (n == 0) {
             return 0;
-        } else if (n == 1){
+        } else if (n == 1) {
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);

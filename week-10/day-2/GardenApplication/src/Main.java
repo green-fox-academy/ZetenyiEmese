fef50ca7e47edf46987c1804c163f5ac86d3f946
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-04 22:40:34 +0100

    class Main with main method completed

---
+++ week-10/day-2/GardenApplication/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Garden garden = new Garden();
+
+        garden.add(new Flower("yellow"));
+        garden.add(new Flower("blue"));
+        garden.add(new Tree("purple"));
+        garden.add(new Tree("orange"));
+
+        System.out.println(garden);
+
+        garden.water(40);
+        System.out.println(garden);
+
+        garden.water(70);
+        System.out.println(garden);
+    }
+}

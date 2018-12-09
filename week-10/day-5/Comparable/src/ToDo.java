88170cf52a5f312f90a0142fa0ae7cc9c4f196f2
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-09 19:09:23 +0100

    added class ToDo implementing Comparable

---
+++ week-10/day-5/Comparable/src/ToDo.java
@@ -0,0 +1,44 @@
+public class ToDo implements Comparable<ToDo> {
+
+    private String description;
+    private boolean completed;
+
+    public ToDo(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    @Override
+    public int compareTo(ToDo otherToDo) {
+        if (completed) {
+            if (!otherToDo.isCompleted()) {
+                return -1;
+            } else {
+                return description.compareTo(otherToDo.getDescription());
+            }
+        } else {
+            if (otherToDo.isCompleted()) {
+                return 1;
+            } else {
+                return description.compareTo(otherToDo.getDescription());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[x] " : "[ ] ") + description;
+    }
+
+}

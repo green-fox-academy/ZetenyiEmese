d3cb4701b83466fd95e2cf3edb27f4023be29dd8
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-09 23:03:59 +0100

    corrected method refill and added getStatus

--- week-10/day-2/AircraftCarrier/src/Aircraft.java
+++ week-10/day-2/AircraftCarrier/src/Aircraft.java
@@ -1,15 +1,18 @@
 public class Aircraft {
 
+    private static String type;
     private static int maxAmmo;
     private static int baseDamage;
+    private static boolean priority;
     private int ammo;
 
     public Aircraft() {
         this.ammo = 0;
     }
 
-    public int getAmmo() {
-        return ammo;
+    //in order to reach the static field type in the subclass with dynamic binding
+    public String getType() {
+        return type;
     }
 
     //in order to reach the static field maxAmmo in the subclass with dynamic binding
@@ -22,6 +25,15 @@ public class Aircraft {
         return baseDamage;
     }
 
+    //in order to reach the static field priority in the subclass with dynamic binding
+    public boolean isPriority() {
+        return priority;
+    }
+
+    public int getAmmo() {
+        return ammo;
+    }
+
     public int fight() {
         int damage = ammo * getBaseDamage();
         ammo = 0;
@@ -29,11 +41,12 @@ public class Aircraft {
     }
 
     public int refill(int ammoAmount) {
-        if (ammo < getMaxAmmo()){
-            boolean isEnough = ammoAmount > getMaxAmmo() - ammo;
+        int ammoNeed = getMaxAmmo() - ammo;
+        if (ammoNeed > 0){
+            boolean isEnough = ammoAmount > ammoNeed;
             if (isEnough) {
-                ammo = getMaxAmmo();
-                return ammoAmount - (getMaxAmmo() - ammo);
+                ammo += ammoNeed;
+                return ammoAmount - ammoNeed;
             } else {
                 ammo += ammoAmount;
                 return 0;
@@ -41,6 +54,11 @@ public class Aircraft {
         } else {
             return ammoAmount;
         }
+    }
+
+    public String getStatus() {
+        int allDamage = ammo * getBaseDamage();
+        return "Type: " + getType() + ", Ammo: " + ammo + ", Base Damage: " + getBaseDamage() + ", All Damage: " + allDamage;
     }
 
 }

0562b157ba6741d2583bd63fb666a9c77a978b3f
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-10 22:56:25 +0100

    added subclass ElectricGuitar

---
+++ week-10/day-5/StringedInstruments/src/main/java/music/ElectricGuitar.java
@@ -0,0 +1,18 @@
+package main.java.music;
+
+public class ElectricGuitar extends StringedInstrument {
+
+    public ElectricGuitar() {
+       this(6);
+    }
+
+    public ElectricGuitar(int numberOfStrings) {
+        super("Electric Guitar", numberOfStrings);
+    }
+
+    @Override
+    public String sound() {
+        return "Twang";
+    }
+
+}

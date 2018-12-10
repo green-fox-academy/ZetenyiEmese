691f7a0188e83ef96524b361e04c8889c6973683
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-11 00:40:57 +0100

    added abstract class Animal

---
+++ week-10/day-5/Flyable/src/Animal.java
@@ -0,0 +1,38 @@
+public abstract class Animal{
+
+    private String name;
+    private int age;
+    private String gender;
+    private boolean isHungry;
+    private boolean isAsleep;
+
+    public Animal() {
+    }
+
+    public Animal(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.isHungry = true;
+        this.isAsleep = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+
+    public String move() {
+        return "moving";
+    }
+
+    public abstract String breed();
+
+}

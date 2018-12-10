d4b66b5f729f7a309ff007c80a26cef3094a3bb6
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-11 00:42:29 +0100

    added abstract class Vehicle

---
+++ week-10/day-5/Flyable/src/Vehicle.java
@@ -0,0 +1,11 @@
+public abstract class Vehicle {
+
+    private String type;
+    private String brand;
+    private int maxSpeed;
+
+    //public abstract void steer();
+    //public abstract void accelerate();
+    //public abstract void brake();
+
+}

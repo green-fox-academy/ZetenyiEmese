0a8f7ff96de9d6384fd98a521ec3bd25b115d86e
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-10 22:56:47 +0100

    added subclass BassGuitar

---
+++ week-10/day-5/StringedInstruments/src/main/java/music/BassGuitar.java
@@ -0,0 +1,18 @@
+package main.java.music;
+
+public class BassGuitar extends StringedInstrument {
+
+    public BassGuitar() {
+        this(4);
+    }
+
+    public BassGuitar(int numberOfStrings) {
+        super("Bass Guitar", numberOfStrings);
+    }
+
+    @Override
+    public String sound() {
+        return "Duum-duum-duum";
+    }
+
+}

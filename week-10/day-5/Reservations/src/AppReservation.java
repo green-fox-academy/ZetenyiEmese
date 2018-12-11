3d5a2bb519d93857ff47c12b737281f4222d07b5
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-11 23:48:06 +0100

    added AppReservation class with main method

---
+++ week-10/day-5/Reservations/src/AppReservation.java
@@ -0,0 +1,27 @@
+import reservations.Reservation;
+
+import java.util.ArrayList;
+
+public class AppReservation {
+
+    public static void main(String[] args) {
+        ArrayList<Reservation> reservations = new ArrayList<>();
+
+        for (int i = 0; i < 10 ; i++) {
+            reservations.add(new Reservation());
+        }
+
+        System.out.println(reservations.get(0).getCodeBooking());
+        System.out.println(reservations.get(0).getDowBooking());
+
+        display(reservations);
+    }
+
+    public static void display(ArrayList<Reservation> reservations) {
+
+        for (Reservation res : reservations) {
+            System.out.println(res);
+        }
+    }
+
+}

938f0e0d7aa2019ebd79f03f0f1f78606a3e5fe8
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-13 22:59:10 +0100

    test(CountLettersTest): created unit tests for countLetters

---
+++ week-11/day-3/CountLetters/test/CountLettersTest.java
@@ -0,0 +1,51 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+    public static CountLetters countLetters;
+
+    @BeforeClass
+    public static void init() {
+        countLetters = new CountLetters();
+    }
+
+    @Test
+    public void testCountLetters() {
+        String string = "banana";
+        HashMap<Character, Integer> expected = new HashMap<>();
+        expected.put('b', 1);
+        expected.put('a', 3);
+        expected.put('n', 2);
+
+        HashMap<Character, Integer> result = countLetters.countLetters(string);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCountLettersWithEmptyString() {
+        String string = "";
+        HashMap<Character, Integer> expected = new HashMap<>();
+
+        HashMap<Character, Integer> result = countLetters.countLetters(string);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCountLettersWithNull() {
+        CountLetters countLetters = new CountLetters();
+        String string = null;
+        HashMap<Character, Integer> expected = new HashMap<>();
+
+        HashMap<Character, Integer> result = countLetters.countLetters(string);
+
+        assertEquals(expected, result);
+    }
+
+}

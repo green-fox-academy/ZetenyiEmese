5a026301a3716599fbc3b7709d82fee523cf38fd
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-16 00:39:35 +0100

    added class Fibonacci

---
+++ week-11/day-5/Fibonacci/src/Fibonacci.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class Fibonacci {
+
+    //public static double fibonacci(int n) {
+    //    if (n < 1) return 0;
+    //    if (n == 1) return 1;
+    //    return fibonacci(n-2) + fibonacci(n-1);
+    //}
+
+    private HashMap<Integer, Long> hm;
+
+    public Fibonacci() {
+        hm = new HashMap<>();
+        hm.put(0, 0L);
+        hm.put(1, 1L);
+    }
+
+    public long countFibonacci(int n) {
+        if (n < 0) return 0;
+
+        if (hm.containsKey(n)) {
+            return hm.get(n);
+        } else {
+            long fibN = countFibonacci(n-2) + countFibonacci(n-1);
+            hm.put(n, fibN);
+            return fibN;
+        }
+    }
+
+    //public static void main(String[] args) {
+    //    Fibonacci fib = new Fibonacci();
+    //    System.out.println(fib.countFibonacci(50));
+    //}
+
+}

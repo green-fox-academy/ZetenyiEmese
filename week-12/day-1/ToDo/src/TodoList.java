75e5a7fab4ac11d5965789e68ab13dd3c1d5aea2
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-18 00:27:45 +0100

    added TodoList class

--- week-12/day-1/ToDo/src/TodoList.java
+++ week-12/day-1/ToDo/src/TodoList.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class TodoList {
+public class TodoList implements Serializable {
+
+    private ArrayList<Todo> list;
+
+    public TodoList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(Todo todo) {
+        list.add(todo);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for(int i = 0; i < list.size(); i++) {
+            result += (i+1) + " - " + list.get(i) + "\n";
+        }
+        return result;
+    }
+
 }

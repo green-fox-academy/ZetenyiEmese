385614b8bd7cc4b7e74ff2fdcfd9916c18422fbf
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-18 15:27:37 +0100

    added methods removeTask and checkTask

--- week-12/day-1/ToDo/src/Manipulator.java
+++ week-12/day-1/ToDo/src/Manipulator.java
@@ -7,8 +7,30 @@ public class Manipulator {
         FileIO.writeTodoListToFile("todos.txt", todoList);
     }
 
-    public static void removeTask(String index) {
-        //TODO
+    public static void removeTask(String whichTask) {
+        TodoList todoList = FileIO.readTodoListFromFile("todos.txt");
+
+        try {
+            int which = Integer.parseInt(whichTask);
+            todoList.remove(which);
+        } catch (NumberFormatException e) {
+            System.out.println("Unable to remove: index is not a number");
+        }
+
+        FileIO.writeTodoListToFile("todos.txt", todoList);
+    }
+
+    public static void checkTask(String whichTask) {
+        TodoList todoList = FileIO.readTodoListFromFile("todos.txt");
+
+        try {
+            int which = Integer.parseInt(whichTask);
+            todoList.check(which);
+        } catch (NumberFormatException e) {
+            System.out.println("Unable to check: index is not a number");
+        }
+
+        FileIO.writeTodoListToFile("todos.txt", todoList);
     }
 
 }

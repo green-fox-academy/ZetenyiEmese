9fc1488552ab46ae67dc041489494902514a3b93
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-18 15:40:06 +0100

    added the check task feature and argument error handling

--- week-12/day-1/ToDo/src/TodoApp.java
+++ week-12/day-1/ToDo/src/TodoApp.java
@@ -1,10 +1,12 @@
-import java.io.*;
-
 public class TodoApp {
 
     public static void main(String[] args) {
 
         if (args.length == 0) {
+            Print.printUsage();
+
+        } else if (!isSupportedArgument(args[0])) {
+            System.out.println("Unsupported argument");
             Print.printUsage();
 
         } else if (args[0].equals("-l")) {
@@ -18,9 +20,25 @@ public class TodoApp {
             }
 
         } else if (args[0].equals("-r")) {
-            Manipulator.removeTask(args[1]);
+            if (args.length == 1) {
+                System.out.println("Unable to remove: no index provided");
+            } else {
+                Manipulator.removeTask(args[1]);
+            }
         }
 
+        else if (args[0].equals("-c")) {
+            if (args.length == 1) {
+                System.out.println("Unable to check: no index provided");
+            } else {
+                Manipulator.checkTask(args[1]);
+            }
+        }
+
+    }
+
+    private static boolean isSupportedArgument(String arg) {
+        return arg.equals("-l") || arg.equals("-a") || arg.equals("-r") || arg.equals("-c");
     }
 
 }

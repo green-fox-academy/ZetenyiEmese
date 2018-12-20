ae298c9a41bf46af0a67e627eaebcc6baf37f616
Zétényi Emese <zetenyi.emese@gmail.com>
2018-12-20 22:31:30 +0100

    added class F35 extending Aircraft

---
+++ week-10/day-2/AircraftCarrierAbstract/src/F35.java
@@ -0,0 +1,32 @@
+public class F35 extends Aircraft {
+
+    private static String type = "F35";
+    private static int maxAmmo = 12;
+    private static int baseDamage = 50;
+    private static boolean priority = true;
+
+    //public F35() {
+    //    super();
+    //}
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public int getMaxAmmo() {
+        return maxAmmo;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return baseDamage;
+    }
+
+    @Override
+    public boolean isPriority() {
+        return priority;
+    }
+
+}

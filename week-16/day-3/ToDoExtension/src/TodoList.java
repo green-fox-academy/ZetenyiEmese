1d03da594360b8669e413d7c6bd08b3baa92de60
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-02 20:10:19 +0100

    fix(TodoList): implement createNextTodoId and update methods

---
+++ week-16/day-3/ToDoExtension/src/TodoList.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class TodoList {
+
+  private String fileName;
+  private ArrayList<Todo> list;
+
+  public TodoList() {
+    this.fileName = "todos.txt";
+    this.list = FileIO.readArrayListOfTodosFromFile2(fileName);
+    //this.list = FileIO.readArrayListOfTodosFromFile(fileName);
+  }
+
+  public int createNextTodoId() {
+    int nextTodoId;
+    if (isEmpty()) {
+      nextTodoId = 1;
+    } else {
+      nextTodoId = list.get(list.size() - 1).getId() + 1;
+    }
+    return nextTodoId;
+  }
+
+  public void add(Todo todo) {
+    list.add(todo);
+    FileIO.writeArrayListOfTodosToFile2(fileName, list);
+    //FileIO.writeArrayListOfTodosToFile(fileName, list);
+  }
+
+  public void remove(int which) {
+    try {
+      list.remove(which - 1);
+    } catch (Exception e) {
+      System.out.println("Unable to remove: index is out of bound!");
+    }
+    FileIO.writeArrayListOfTodosToFile2(fileName, list);
+    //FileIO.writeArrayListOfTodosToFile(fileName, list);
+  }
+
+//  public void remove(int id) {
+//    list = (ArrayList<Todo>) list.stream().filter(todo -> todo.getId() != id).collect(Collectors.toList());
+//    FileIO.writeArrayListOfTodosToFile2(fileName, list);
+//  }
+
+  public void check(int which) {
+    try {
+      list.get(which - 1).complete();
+    } catch (Exception e) {
+      System.out.println("Unable to check: index is out of bound!");
+    }
+    FileIO.writeArrayListOfTodosToFile2(fileName, list);
+    //FileIO.writeArrayListOfTodosToFile(fileName, list);
+  }
+
+  public void update(int id, String newDescription) {
+    for (Todo todo : list) {
+      if (todo.getId() == id) {
+        todo.setDescription(newDescription);
+        FileIO.writeArrayListOfTodosToFile2(fileName, list);
+        //FileIO.writeArrayListOfTodosToFile(fileName, list);
+        return;
+      }
+    }
+
+    System.out.println("Unable to update: id is not found!");
+  }
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public boolean hasUndoneTasks() {
+    for (Todo todo : list) {
+      if (!todo.isCompleted()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public String getUndoneTasks() {
+    String result = "";
+    int i = 0;
+
+    for (Todo todo : list) {
+      if (!todo.isCompleted()) {
+        result += (i + 1) + " - " + todo.toString() + "\n";
+        i++;
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    String result = "";
+
+    for (int i = 0; i < list.size(); i++) {
+      result += (i + 1) + " - " + list.get(i).toString() + "\n";
+    }
+
+    return result;
+  }
+
+}

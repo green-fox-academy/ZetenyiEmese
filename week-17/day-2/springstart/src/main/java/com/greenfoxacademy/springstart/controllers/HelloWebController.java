59795e52a6675016ea17cd2dac4e5099286de2fd
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-05 16:46:16 +0100

    feat)Ã(HelloWebController): implement greeting and greeting2 methods greeting the World and You

---
+++ week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy.springstart.controllers;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+        import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class HelloWebController {
+
+  private AtomicLong apiCalls = new AtomicLong(0);
+
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " World");
+    return "greeting";
+  }
+
+  @RequestMapping("/web/greeting2")
+  public String greeting2(Model model) {
+    model.addAttribute("name", " Emese");
+    model.addAttribute("apiCalls", apiCalls.incrementAndGet());
+    return "greeting";
+  }
+
+}

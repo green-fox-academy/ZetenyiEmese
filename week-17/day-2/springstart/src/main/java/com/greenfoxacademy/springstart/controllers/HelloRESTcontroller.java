1b8c62d66dba3dee112f38bfca16f7ac3aec52ba
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-05 13:43:26 +0100

    add greeting Hello Your name

--- week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTcontroller.java
+++ week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTcontroller.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.Greeting;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,12 @@ public class HelloRESTcontroller {
 
   @RequestMapping(value = "/greeting2")
   public Greeting greeting2(@RequestParam String name) {
-    return new Greeting(1, "Hello" + name);
+    return new Greeting(1, "Hello " + name);
+  }
+
+  @RequestMapping(value = "/greeting3/{name}")
+  public Greeting greeting3(@PathVariable String name) {
+    return new Greeting(1, "Hello " + name);
   }
 
 }

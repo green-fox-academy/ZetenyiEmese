bfcab760d3495d7815d544e25ef1b933af299f2b
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-05 12:31:19 +0100

    add greeting Hello World

---
+++ week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTcontroller.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import com.greenfoxacademy.springstart.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTcontroller {
+
+  @RequestMapping(value = "/greeting")
+  public Greeting greeting() {
+    return new Greeting(1, "HelloWorld");
+  }
+
+  @RequestMapping(value = "/greeting2")
+  public Greeting greeting2(@RequestParam String name) {
+    return new Greeting(1, "Hello" + name);
+  }
+
+}

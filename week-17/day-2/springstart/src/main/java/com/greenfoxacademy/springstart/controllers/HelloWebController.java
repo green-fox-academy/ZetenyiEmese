88bf4013798a9091d6169c496d2dda2fbfc6bc7c
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-06 13:24:39 +0100

    feat)(HelloWebController): add method greeting3 getting name param from URL

--- week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-17/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -20,6 +21,13 @@ public class HelloWebController {
   @RequestMapping("/web/greeting2")
   public String greeting2(Model model) {
     model.addAttribute("name", " Emese");
+    model.addAttribute("apiCalls", apiCalls.incrementAndGet());
+    return "greeting";
+  }
+
+  @RequestMapping("/web/greeting3")
+  public String greeting3(Model model, @RequestParam String name) {
+    model.addAttribute("name", name);
     model.addAttribute("apiCalls", apiCalls.incrementAndGet());
     return "greeting";
   }

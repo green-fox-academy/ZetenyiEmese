742295bb5cba361f4212250b20465037e16c5b0d
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-14 23:41:13 +0100

    create Hellobeanworld application and Printer service class autowired to the application

---
+++ week-18/day-4/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy.hellobeanworld;
+
+import com.greenfoxacademy.hellobeanworld.services.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication implements CommandLineRunner {
+
+  @Autowired
+  private Printer printer;
+
+//  Nem szükséges:
+//  @Autowired
+//  public HellobeanworldApplication(Printer printer) {
+//    this.printer = printer;
+//  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanworldApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("Hello Emese!");
+  }
+
+}

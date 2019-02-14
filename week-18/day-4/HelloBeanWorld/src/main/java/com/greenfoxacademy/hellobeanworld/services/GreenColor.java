95ee45017f8e2f23949fc5c975182aba8324435d
Zétényi Emese <zetenyi.emese@gmail.com>
2019-02-15 00:22:04 +0100

    Change the implementations in GreenColor sevice class to use the Printer service with autowiring (instead of sout-ing)

--- week-18/day-4/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/services/GreenColor.java
+++ week-18/day-4/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/services/GreenColor.java
@@ -1,13 +1,18 @@
 package com.greenfoxacademy.hellobeanworld.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreenColor implements MyColor {
 
+  @Autowired
+  private Printer printer;
+
   @Override
   public void printColor() {
-    System.out.println("It is green in color...");
+    printer.log("It is green in color...");
+    //System.out.println("It is green in color...");
   }
 
 }

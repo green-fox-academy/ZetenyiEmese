b78f928e2f7e9230a365ccedd6f40ba8e37f8dff
Zétényi Emese <zetenyi.emese@gmail.com>
2019-03-25 12:48:25 +0100

    correct indentation in class Todo

--- week-22/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodos/models/Todo.java
+++ week-22/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodos/models/Todo.java
@@ -1,9 +1,9 @@
 package com.greenfoxacademy.listingtodos.models;
 
-        import javax.persistence.Entity;
-        import javax.persistence.GeneratedValue;
-        import javax.persistence.GenerationType;
-        import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Todo {
